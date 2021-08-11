89e5aaa14780317cc3127dd4a5f3e72b3eba0fd5
devlautum <sariel_ruby@naver.com>
2021-08-11 23:11:58 +0900

    Edit Module import code
    #5 : require('.ridibooks~') to require(./ridibooks~')

--- crawler/crawler_run.js
+++ crawler/crawler_run.js
@@ -2,13 +2,13 @@
 const joara = require('./joara_crawler.js');
 const naver = require('./naver_series_crawler.js');
 const kakao = require('./kakao_page_crawler.js');
-const ridibooks = require('.ridibooks_crawler.js');
+const ridibooks = require('./ridibooks_crawler.js');
 const fs = require('fs');
 
 // csv 폴더 읽기, 없으면 csv 폴더 생성
 fs.readdir('csv',(err)=>{
     if(err){
-        console.err('csv 폴더가 없으므로, 우선 폴더를 생성합니다.');
+        console.error('csv 폴더가 없으므로, 우선 폴더를 생성합니다.');
         fs.mkdirSync('csv');
     }
 });
