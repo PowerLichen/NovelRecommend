a940464a8d4c9a9781afee0a38c1c96a43505a51
PowerLichen <dltm135@gmail.com>
2021-08-21 14:20:05 +0900

    Add login api & refactoring

---
+++ routes/user.js
@@ -0,0 +1,34 @@
+const passport = require('passport');
+const router = express.Router();
+
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        if (err) {
+            console.log(err);
+            return next(err);
+        }
+        if (info) {
+            return res.status(401).send(info.reason);
+        }
+        return req.login(user, async (loginErr) => {
+            if (loginErr) { return next(loginErr); }
+            return res.json(user);
+        });
+    })(req, res, next);
+});
+
+router.post('/join', (req,res,next)=>{
+    let sql = 'INSERT INTO userdata VALUES (null,?,?,?,now(), 0)';
+    let id = req.body.id;
+    let password = req.body.password;
+    let nickname = req.body.nickname;
+    let params = [id, password, nickname];
+    pool.query(sql, params,
+        (err, results, fields) => {
+            if(err){
+                console.log(err);
+                return next(err);
+            }
+            console.log('Create account end');
+        });
+});
