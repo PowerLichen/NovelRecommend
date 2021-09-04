7fd87b1250cab39344ada7c8366479b015bafd3d
PowerLichen <dltm135@gmail.com>
2021-09-04 21:49:02 +0900

    Add logout, checkid function

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,16 @@
 const express = require('express')
-const passport = require('passport');  
+const passport = require('passport');
 const router = express.Router();
 
 
 module.exports = (pool) => {
+    router.get('/', (req, res) => {
+        if (!req.user) {
+            return res.status(200).json(null);
+        }
+        res.status(200).json(req.user);
+    });
+
     router.post('/login', (req, res, next) => {
         passport.authenticate('local', (err, user, info) => {
             if (err) {
@@ -19,8 +26,29 @@ module.exports = (pool) => {
             });
         })(req, res, next);
     });
-    
-    router.post('/join', (req,res,next)=>{
+
+    router.get('/logout', (req, res) => {
+        req.logout();
+        req.session.destroy();
+        res.send('Complete logout');
+    })
+
+    router.post('/checkId', (req, res, next) => {
+        let sql = 'SELECT idx FROM userdata_tb WHERE user_id=?';
+        //post 요청 시 전달되는 id
+        let id = req.body.id;
+        pool.qurey(sql, [id],
+            (err, results) => {
+                if (err) {
+                    console.log(err);
+                    return next(err);
+                }
+                if (results.length !== 0) res.status(403).send('해당 아이디가 이미 DB에 있습니다.');
+                else res.send("Good ID.");
+            });
+    });
+
+    router.post('/join', (req, res, next) => {
         let sql = 'INSERT INTO userdata VALUES (null,?,?,?,now(), 0)';
         let id = req.body.id;
         let password = req.body.password;
@@ -28,13 +56,14 @@ module.exports = (pool) => {
         let params = [id, password, nickname];
         pool.query(sql, params,
             (err, results, fields) => {
-                if(err){
+                if (err) {
                     console.log(err);
                     return next(err);
                 }
                 console.log('Create account end');
+                res.send("Complete join");
             });
     });
-    
+
     return router
 }
