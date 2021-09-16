4335763bacf3ca1394a7c952016fe0fa58142f7d
PowerLichen <dltm135@gmail.com>
2021-09-17 00:49:34 +0900

    Add novel list return api

---
+++ routes/novel.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router();
+
+module.exports = (pool) => {
+
+    router.get('/lists', (req, res, next) => {
+        //위에서 10개의 데이터를 호출
+        let sql = 'SELECT id,title,imgurl FROM novel_data LIMIT 0,10';
+        pool.query(sql, (err, results) => {
+            if(err){
+                console.log(err);
+                return next(err);
+            }
+            res.send(results);
+        });
+    });
+
+
+
+    return router;
+}
