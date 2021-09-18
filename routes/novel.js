b621fade05b3353875e1cbd0a81f52d49758f03c
PowerLichen <dltm135@gmail.com>
2021-09-18 10:51:48 +0900

    Add get novel infodata API

--- routes/novel.js
+++ routes/novel.js
@@ -1,11 +1,12 @@
 const express = require('express')
+const path = require('path');
 const router = express.Router();
 
 module.exports = (pool) => {
 
     router.get('/lists', (req, res, next) => {
         //위에서 10개의 데이터를 호출
-        let sql = 'SELECT id,title,imgurl FROM novel_data LIMIT 0,10';
+        const sql = 'SELECT id,title,imgurl FROM novel_data LIMIT 0,10';
         pool.query(sql, (err, results) => {
             if (err) {
                 console.log(err);
@@ -16,8 +17,25 @@ module.exports = (pool) => {
     });
 
     // 작품 상세정보 API : '/novel/detail/:id'
-    router.get('/detail/:novel_id', (req, res, next) => {
-
+    router.get('/noveldata/:novel_id', (req, res, next) => {
+        //패러미터 파싱
+        const novel_id = path.parse(req.params.novel_id).base;
+        //메인 쿼리
+        //id, title, imgurl, genre, description, author_id, name
+        const data_sql = 'SELECT d.id, d.title, d.imgurl,\
+                d.genre, d.description, d.author_id, au.name \
+                FROM novel_data AS d JOIN author AS au \
+                ON d.author_id = au.id WHERE d.id=?;';
+        const link_sql = 'SELECT url FROM novel_link WHERE nid = ?';
+        pool.query(data_sql+link_sql, [novel_id,novel_id],
+            (err, results) => {
+                if (err) {
+                    console.log(err);
+                    return next(err);
+                }
+                const result_data = {...results[0][0], urls:results[1]};
+                res.send(result_data);
+            });
     });
 
 
