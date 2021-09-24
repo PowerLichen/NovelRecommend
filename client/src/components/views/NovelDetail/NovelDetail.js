d50a46d39f9c1593a2b2c7d9ba65694c370712a1
slave13 <hskim2468@gmail.com>
2021-09-24 12:49:14 +0900

    correct err

--- client/src/components/views/NovelDetail/NovelDetail.js
+++ client/src/components/views/NovelDetail/NovelDetail.js
@@ -6,12 +6,43 @@ import NovelInfo from "./Sections/NovelInfo";
 //작품 페이지 출력
 function NovelDetail(props) {
 
-    const [Novel, setMovie] = useState([])
+    const [Novel, setMovie] = useState([]);
+    //const [url, setUrl] = useState((window.location.pathname).slice(7));
+    const url = useState((window.location.pathname).slice(7));
 
+    // const urlsplit = (event) => {
+    //     url = window.location.pathname;
+    //     url = url.slice(6);
+    //     console.log(url);
+    // }
+    
+    // function urlspliting() {
+    //     url = window.location.pathname;
+    //     url = url.slice(6);
+    //     console.log(url);
+    // }
+
+    // const urlHandler = ()=>{
+    //     url = window.location.pathname;
+    //     url = url.slice(6);
+    //     console.log(url);
+    // }
+
+    useEffect(() => {
+        //console.log(url);
+
+    });
+      
     return (
+        <div>
+        <NovelInfo id = {url}></NovelInfo>
         
-        <NovelInfo></NovelInfo>
-
+        안녕
+        
+        {url}
+        
+        </div>
+        
     )
 }
 
