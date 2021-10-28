fac266724833c6602e7c0cf7f042a579e11807b0
PowerLichen <dltm135@gmail.com>
2021-10-28 22:41:42 +0900

    소설 검색 코드 수정

--- routes/search.js
+++ routes/search.js
@@ -5,14 +5,16 @@ const router = express.Router();
 module.exports = (pool) => {
     //단어 검색
     //API: '/search/keyword/천마/568'
-    router.get('/keyword/:keyword', (req, res, next) => {
-        const keyword = path.parse(req.params.keyword);
+    router.get('/keyword/:keyword/:id', (req, res, next) => {
+        const keyword = path.parse(req.params.keyword).base;
+        const pageId = path.parse(req.params.id).base * 20;
+        const queryword = "%" + keyword + "%";
         const sql =
-            'SELECT id,title,imgurl \
+            'SELECT id, title, imgurl \
             FROM novel_data \
-            WHERE title LIKE "%?%" OR description LIKE "%?%" \
-            LIMIT 0,20';
-        pool.query(sql, [keyword, keyword],
+            WHERE title LIKE ? OR description LIKE ? \
+            LIMIT ?, 20';
+        pool.query(sql, [queryword, queryword, pageId],
             (err, results) => {
                 if (err) {
                     console.log(err);
