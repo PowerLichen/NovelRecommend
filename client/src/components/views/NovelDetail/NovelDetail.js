58eb3835beade8076186048e78354dc97959fc99
slave13 <hskim2468@gmail.com>
2021-10-31 10:41:26 +0900

    작품별 점수 매기기 기능 추가

--- client/src/components/views/NovelDetail/NovelDetail.js
+++ client/src/components/views/NovelDetail/NovelDetail.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Row, Button } from 'antd';
 import axios from "axios";
 import NovelInfo from "./Sections/NovelInfo";
+import RatingPage from "../RatingPage/RatingPage";
 
 //작품 페이지 출력
 function NovelDetail(props) {
@@ -36,6 +37,7 @@ function NovelDetail(props) {
     return (
         <div>
         <NovelInfo id = {url}></NovelInfo>
+        <RatingPage id = {url}></RatingPage>
         </div>
         
     )
--- client/src/components/views/RatingPage/RatingPage.js
+++ client/src/components/views/RatingPage/RatingPage.js
@@ -1,34 +1,82 @@
 import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import ReactDOM from 'react-dom';
 import StarRatingComponent from 'react-star-rating-component';
+import axios from "axios";
+import { USER_SERVER } from '../../../components/Config.js';
+
+import { Descriptions } from 'antd';
+
  
 function RatingPage(props) {
   
-    const [rating, setRating] = useState([4]);
+    //별점 점수
+    const [Score, setScore] = useState([4]);
+    //회원 idx
+    const [Idx, setIdx] = useState([0]);
+
+    //유저 정보
+    const user = useSelector(state => state.user)
+    console.log(user.userData)
     
- 
+    //별점 클릭
     const onStarClick = (nextValue) => {
-        setRating(nextValue);
+      setScore(nextValue);
     }
 
+    useEffect(() => {
+      //setIdx(user.userData.idx);
+    }, [])
+
+
+    //점수 제출
+    const submitScore = () => {
+      axios
+        .get(`${USER_SERVER}/rating/addscore/${user.userData.idx}/${props.id}/${Score}`) 
+        .then((data)=>{console.log(data);})
+        //.then(({ data }) => {setPosts(data)});
+
+      // fetch(`${USER_SERVER}/rating/addscore/20/83222/5`) //`${USER_SERVER}/rating/addscore/${user.userData.idx}/${props.id}/${Score}`
+      // .then(response => response.json())
+      //       .then(response => {
+      //           console.log(response)
+      //           //setScore([...Score, ...response])
+      //         })
+    }
 
     return (         
-      <div >
+      <div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
         <br></br>
         <br></br>
         <br></br>
         <br></br>
         <br></br>
 
-        <h2>평점 선택 : {rating}</h2>
+        
+        {user.userData &&(
+          <Descriptions.Item label="My ID">
+              {user.userData.idx}
+              
+          </Descriptions.Item>
+        )}
+        <br></br>
+        {props.id}      
+        <h2>평점 선택 : {Score}</h2>
         <StarRatingComponent 
           name="rate1" 
           starCount={5}
-          value={rating}
+          value={Score}
           onStarClick={onStarClick}
         />
         <br></br>
-        <input type='submit'></input>
+        <div style={{ display: 'flex' }}>
+          <button onClick={submitScore}>제출</button>
+        </div>
         
       </div>
         
@@ -37,4 +85,4 @@ function RatingPage(props) {
   
 }
 
-export default RatingPage
+export default RatingPage;
