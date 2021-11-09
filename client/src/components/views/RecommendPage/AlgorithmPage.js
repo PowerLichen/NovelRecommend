ec33c12962595031d71f623fc301b544d00df73f
slave13 <hskim2468@gmail.com>
2021-11-10 04:47:57 +0900

    수정

--- client/src/components/views/RecommendPage/AlgorithmPage.js
+++ client/src/components/views/RecommendPage/AlgorithmPage.js
@@ -19,10 +19,6 @@ function AlgorithmPage(props) {
     if (user.userData === undefined) {
         return
     }
-    else{
-      alert('로그인이 필요합니다. 로그인 페이지로 이동합니다.')
-      props.history.push('/login');
-    }
     axios
       .get(`${USER_SERVER}/novel/content-rec/${user.userData.idx}`)
       .then(({ data }) => { setPosts(data); console.log(data);});
