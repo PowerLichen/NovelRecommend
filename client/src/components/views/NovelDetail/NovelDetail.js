a7282a0578ffec8e52c8ff24963f6b0f64d54f9f
slave13 <hskim2468@gmail.com>
2021-11-13 18:21:54 +0900

    수정

--- client/src/components/views/NovelDetail/NovelDetail.js
+++ client/src/components/views/NovelDetail/NovelDetail.js
@@ -3,13 +3,13 @@ import NovelInfo from "./Sections/NovelInfo";
 
 //작품 페이지 출력
 function NovelDetail(props) {
+    
+    let nid = props.match.params.id;
 
-    const nidUrl = useState((window.location.pathname).slice(7));
- 
     return (
         <div style={{ width: '800px', margin: '1rem auto' }}>
             <div>
-            <NovelInfo nid = {nidUrl}></NovelInfo>
+            <NovelInfo nid = {nid}></NovelInfo>
             </div>
         </div>
         
